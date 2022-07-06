5ff147b1efcb9a5a7094a4a5b9584773ef310cb8
Emil212 <hceronj1400@alumno.ipn.mx>
2022-07-05 22:53:12 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+// import { ContadorComponent } from './contador/contador/contador.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // ContadorComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, HeroesModule, ContadorModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+})
+export class ListadoComponent {
+  heroes: string[] = ['Spiderman', 'Ironman', 'Thor', 'Hulk'];
+  heroeBorrado: string = '';
+
+  borrarHeroe() {
+    console.log('Borrando...');
+    this.heroeBorrado = this.heroes.shift() || '';
+  }
+}
